2b389cac2daa3c397c617e811750a8b6e999e977
arisafaribinridwan <arisafari.ridwan@gmail.com>
2022-10-14 22:31:59 +0700

    add javascript code

---
+++ script/script.js
@@ -0,0 +1,22 @@
+const wrapper = document.getElementById("wrapper")
+const modal = document.getElementById("modal")
+const imageModal = document.getElementById("imageDialog")
+const titleModal = document.getElementById("titleDialog")
+const descDialog = document.getElementById("descDialog")
+const ourproduct = document.getElementById("ourProducts")
+
+
+function getDetail(nomor) {
+    // console.log('Product No;', nomor)
+    const title = document.getElementById("prodTitle" + nomor);
+    imageModal.src = "assets/nike" + nomor + ".png"
+    titleModal.textContent = title.textContent
+
+    ourproduct.style.display = "none"
+    modal.style.display = "block"
+}
+
+function closeDialog() {
+    modal.style.display = "none"
+    ourproduct.style.display = "block"
+}
